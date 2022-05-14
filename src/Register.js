1f2d24186e9b325b3696bd9fd2d4de66860cff8e
Keiji-morita <keijim3116@gmail.com>
2022-05-14 18:38:39 +0900

    最初のページ

---
+++ src/Register.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from "axios";
+
+export default function Register() {
+  return (
+    <main>
+    <div>
+      <h1>ログイン・ユーザー情報登録</h1>
+      <form>
+        <input />
+        <label>ニックネーム</label>
+      </form>
+      
+    </div>
+    </ main>
+  )
+}
+
