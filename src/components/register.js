dc7430a9e30a617f6126509e51b6bfc1cfbc3010
Keiji-morita <keijim3116@gmail.com>
2022-05-15 03:39:55 +0900

    サインイン実装中

--- src/components/register.js
+++ src/components/register.js
@@ -4,14 +4,33 @@ import axios from "axios";
 
 
 export default function Register() {
+  // registerUrl = 'http://localhost:8000/api/admin/register'
+	// state = {
+	// 	redirect: false
+	// }
+    
+  const registUser = {
+    methods: 'POST',
+  }
   
   return (
     <main>
     <div>
       <h1>ユーザー情報登録</h1>
+      
       <form>
+        <label>ユーザーネーム</label>
         <input />
-        <label>ニックネーム</label>
+      </form>
+
+      <form>
+        <label>メールアドレス</label>
+        <input />
+      </form>
+
+      <form>
+        <label>パスワード</label>
+        <input />
       </form>
       
     </div>
---
+++ src/models/user.js
@@ -0,0 +1,10 @@
+// name: String
+// email: String
+// password: String
+
+
+// {
+//   "name"; "string",
+//   "email"; "string",
+//   "password" "string"
+// }
