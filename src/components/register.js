4f0746006b36cf39fea83a83a6f4a95320e1e506
Keiji-morita <keijim3116@gmail.com>
2022-05-15 22:22:17 +0900

    ログイン実装

--- src/components/register.js
+++ src/components/register.js
@@ -1,44 +1,107 @@
-import React from 'react'
-import axios from "axios";
+import { useEffect, useState } from "react";
+// import "./App.css";
 
+function App() {
+const initialValues = { username: "", mailAddress: "", password: "" };
+const [formValues, setFormValues] = useState(initialValues);
+const [formErros, setFormErrors] = useState({});
+const [isSubmit, setIsSubmit] = useState(false);
 
+const handleChange = (e) => {
+    // console.log(e.target.name);
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    // console.log(formValues);
+};
 
-export default function Register() {
-  // registerUrl = 'http://localhost:8000/api/admin/register'
-	// state = {
-	// 	redirect: false
-	// }
-    
-  const registUser = {
-    methods: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({ title: 'User Information' })
-  };
-  fetch('api-for-missions-and-railways.herokuapp.com')
-  
-  return (
-    <main>
-    <div>
-      <h1>ユーザー情報登録</h1>
-      
-      <form>
-        <label>ユーザーネーム</label>
-        <input />
-      </form>
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+};
 
-      <form>
-        <label>メールアドレス</label>
-        <input />
-      </form>
+useEffect(() => {
+    console.log(formErros);
+    //エラーなしでかつ送信されているなら。
+    if (Object.keys(formErros).length === 0 && isSubmit) {
+    console.log(formValues);
+    } else {
+    }
+}, [formErros]);
 
-      <form>
-        <label>パスワード</label>
-        <input />
-      </form>
-      
-      <button>submit</button>
+  //バリデーションチェック
+const validate = (values) => {
+    const errors = {};
+    //半角英数字のみ(空文字OK)
+    const regex =
+    /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+    //valueが空ならerrrosの配列に入れる。
+    if (!values.username) {
+    errors.username = "ユーザー名を入力してください。";
+    }
+    if (!values.mailAddress) {
+    errors.mailAddress = "メールアドレスを入力してください。";
+    } else if (!regex.test(values.mailAddress)) {
+    errors.mailAddress = "正しいメールアドレスを入力してください";
+    }
+    if (!values.password) {
+    errors.password = "パスワードを入力してください。";
+    } else if (values.password.length < 4) {
+    errors.password = "4文字以上15文字以下のパスワードを入力してください";
+    } else if (values.password.length > 15) {
+    errors.password = "4文字以上15文字以下のパスワードを入力してください";
+    }
+    return errors;
+};
+
+return (
+    <div className="formContainer">
+    <form onSubmit={handleSubmit}>
+        <h1>新規登録</h1>
+        <hr />
+        <div className="uiForm">
+        <div className="formField">
+            <label>ユーザー名</label>
+            <input
+            type="text"
+            name="username"
+            placeholder="ユーザー名"
+            value={formValues.username}
+            onChange={(e) => handleChange(e)}
+            />
+        </div>
+        <p className="errorMsg">{formErros.username}</p>
+        <div className="formField">
+            <label>メールアドレス</label>
+            <input
+            type="text"
+            name="mailAddress"
+            placeholder="メールアドレス"
+            value={formValues.mailAddress}
+            onChange={(e) => handleChange(e)}
+            />
+        </div>
+        <p className="errorMsg">{formErros.mailAddress}</p>
+
+        <div className="formField">
+            <label>パスワード</label>
+            <input
+            type="text"
+            name="password"
+            placeholder="パスワード"
+            value={formValues.password}
+            onChange={(e) => handleChange(e)}
+            />
+        </div>
+        <p className="errorMsg">{formErros.password}</p>
+        <button className="submitButton">登録</button>
+        {Object.keys(formErros).length === 0 && isSubmit && (
+            <div className="msgOk">ログインに成功しました</div>
+        )}
+        </div>
+    </form>
     </div>
-    </ main>
-  )
+);
 }
 
+export default App;
